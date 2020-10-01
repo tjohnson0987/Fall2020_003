01840594ac11d9febeee8f1c6376136848e474a2
owner <owner@192.168.1.145>
2020-10-01 18:12:37 -0500

    I have added another commit.

--- mmb641-lab5/src/TravelDestination.java
+++ mmb641-lab5/src/TravelDestination.java
@@ -2,7 +2,8 @@
 public class TravelDestination {
 	
 	public static void main (String[] args) {
-		System.out.println("helloWorld");
+		System.out.println("HelloWorld");
+		System.out.println("My name is Tinesha Johnson and my hobby is playing basketball. ");
 	}
 
 }
